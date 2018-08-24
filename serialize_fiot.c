84788e7d5ed8cc6680e3dbb8b000f2395280dedc
Evgenii Potapov <potapov@rutoken.ru>
2018-08-24 07:22:28 -0700

    Continue serializing (dev stage)

--- serialize_fiot.c
+++ serialize_fiot.c
@@ -8,7 +8,7 @@
 /* Serializing unsingned short to LengthShortInt */
 void serLengthShortInt(Octet *length, unsigned short number){
     length[1] = number >> 8;
-    (length[0]) = number & 0xff;
+    length[0] = number & 0xff;
 }
 /* Serializing EllipticCurvePoint structure */
 void serEllipticCurvePoint(OctetString *serpt , EllipticCurvePoint *curve){
@@ -16,8 +16,8 @@ void serEllipticCurvePoint(OctetString *serpt , EllipticCurvePoint *curve){
 /* ID field is one Octet length */
     *serpt = realloc(*serpt,  sizeof(Octet) * (len * 2 + 1));
     memcpy(*serpt, &(curve->id), sizeof(Octet));
-    memcpy(*serpt+1, curve->x, len);
-    memcpy(*serpt+1+len, curve->y, len);
+    memcpy(*serpt + 1, curve->x, len);
+    memcpy(*serpt + 1 + len, curve->y, len);
 }
 /* Serializing PreSharedKeyID structure */
 void serPreSharedKeyID(OctetString *keyid, PreSharedKeyID *pskID){
@@ -27,10 +27,11 @@ void serPreSharedKeyID(OctetString *keyid, PreSharedKeyID *pskID){
     else{
         *keyid = realloc(*keyid, sizeof(Octet) * (2 + pskID->length));
         memcpy(*keyid, &(pskID->present), sizeof(Octet));
-        memcpy(*keyid+1, &(pskID->length), sizeof(Octet));
-        memcpy(*keyid+2, pskID->id, pskID->length);
+        memcpy(*keyid + 1, &(pskID->length), sizeof(Octet));
+        memcpy(*keyid + 2, pskID->id, pskID->length);
     }
 }
+/* Serializing IntegrityCode structure */
 void serIntegrityCode(OctetString *keyid, IntegrityCode *icode){
     if (icode->present == notPresent){
         memcpy(*keyid, &(icode->present), sizeof(Octet));
@@ -38,7 +39,20 @@ void serIntegrityCode(OctetString *keyid, IntegrityCode *icode){
     else{
         *keyid = realloc(*keyid, sizeof(Octet) * (2 + icode->length));
         memcpy(*keyid, &(icode->present), sizeof(Octet));
-        memcpy(*keyid+1, &(icode->length), sizeof(Octet));
-        memcpy(*keyid+2, icode->code, icode->length);
+        memcpy(*keyid + 1, &(icode->length), sizeof(Octet));
+        memcpy(*keyid + 2, icode->code, icode->length);
     }
+}
+/* Serializing Frame structure */
+void serFrame(OctetString *serframe, Frame *frame){
+    OctetString icode_serialized = malloc(sizeof(Octet));
+    serIntegrityCode(&icode_serialized, &(frame->icode));
+    unsigned short frame_len = frame->length[1];
+    frame_len = (frame_len << 8) & frame->length[0];
+    *serframe = realloc(*serframe, sizeof(Octet) * frame_len);
+    memcpy(*serframe, &(frame->tag), sizeof(Octet));
+    memcpy(*serframe + 1, &(frame->length), sizeof(Octet) * 2);
+    memcpy(*serframe + 3, &(frame->number), sizeof(Octet) * 5);
+
+
 }
--- serialize_fiot.h
+++ serialize_fiot.h
@@ -2,3 +2,4 @@ void serLengthShortInt(Octet *length, unsigned short number);
 void serEllipticCurvePoint(OctetString *point, EllipticCurvePoint *curve);
 void serPreSharedKeyID(OctetString *keyid, PreSharedKeyID *pskID);
 void serIntegrityCode(OctetString *keyid, IntegrityCode *icode);
+void serFrame(OctetString *serframe, Frame *frame);
