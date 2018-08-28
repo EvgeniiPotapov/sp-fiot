ef9b26644fc20676f4fbdb9c5a55437c19f3ed08
Evgenii Potapov <potapov@rutoken.ru>
2018-08-28 00:08:29 -0700

    Add Frame and ClientHelloMessage serializing

--- main.c
+++ main.c
@@ -6,15 +6,33 @@
 #include "serialize_fiot.h"
 
 int main(){
-    IntegrityCode a;
-    OctetString c = "abcdef";
-    a.present = isPresent;
-    a.length = 6;
-    a.code = c;
-    OctetString b = malloc(sizeof(Octet));
-    serIntegrityCode(&b,&a);
+    OctetString random = "12345678900987654321123456789033";
+    ClientHelloMessage hello;
+    hello.algorithm = streebog512;
+    memcpy(hello.random, random, 32);
+    EllipticCurvePoint point;
+    point.id = id_tc26_gost3410_2012_256_paramsetA;
+    point.x = "6655";
+    point.y = "8877";
+    PreSharedKeyID ipsk;
+    ipsk.present = notPresent;
+    ipsk.length = 3;
+    ipsk.id = "994";
+    PreSharedKeyID epsk;
+    epsk.present = isPresent;
+    epsk.length = 2;
+    epsk.id = "73";
+    hello.point = point;
+    hello.iPSK = ipsk;
+    hello.ePSK = epsk;
+    hello.countOfExtensions = 4;
+    OctetString serhello = malloc(sizeof(Octet));
+    serClientHelloMessage(&serhello, &hello);
     int i;
-    for(i=0;i<10;i++) printf("%x\n",b[i]);
+    for(i=0;i<49;i++) printf("%x\n",serhello[i]);
+    free(serhello);
+
+
 
 }
 
--- serialize_fiot.h
+++ serialize_fiot.h
@@ -3,3 +3,4 @@ void serEllipticCurvePoint(OctetString *point, EllipticCurvePoint *curve);
 void serPreSharedKeyID(OctetString *keyid, PreSharedKeyID *pskID);
 void serIntegrityCode(OctetString *keyid, IntegrityCode *icode);
 void serFrame(OctetString *serframe, Frame *frame);
+void serClientHelloMessage(OctetString *clienthello, ClientHelloMessage *clientmessage);
