75ac5e2afcaa420b3f9fb0acf241f5095853e9f3
Evgenii Potapov <allforblender@mail.ru>
2018-10-14 16:39:21 +0500

    End VKO

--- fiot_include/gench.h
+++ fiot_include/gench.h
@@ -5,3 +5,4 @@ OctetString check_verify_frame(OctetString buf, OctetString eSHTK, OctetString i
 OctetString gen_CHTS(OctetString buf, OctetString hello, OctetString s_hello, OctetString R1, OctetString H3);
 OctetString genVerify(OctetString H4);
 OctetString genVerifyFrame(OctetString verify, OctetString eSHTK, OctetString iSHTK);
+void make_session_keys(OctetString xQ, OctetString R2, OctetString H5, OctetString SATS, OctetString CATS);
